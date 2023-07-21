88bd905a1b7080547d712317c0540cef64de4a20
RayhanAdii <rayhan.a.wicaksono@gmail.com>
2023-07-21 19:53:42 +0700

    learn class components, also add comment for functional components

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
+// Implementing Functional Components
+// Pro: Simple, easy, solution without using state
+// Use as much as possible
 
+// Can use this function
 //function Greet(){
 //    return <h1>Hello Rayhan</h1>
 //}
 
+//Also this
 const Greet = () => <h1>Hello Rayhann</h1>
 
 
---
+++ hello-world/src/components/Welcome.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+// Implementing Class components
+// State
+// Complex UI logic
+// Provide lifecycle hooks
+
+class Welcome extends Component {
+    render(){
+        return <h1>Class Component</h1>
+    }
+
+}
+
+export default Welcome
