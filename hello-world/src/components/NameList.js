7f4396ed89ea4fcb246d542d49820a8637bbf8eb
RayhanAdii <rayhan.a.wicaksono@gmail.com>
2023-08-02 22:55:55 +0700

    learn list rendering part 2, file: NameList.js

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -1,15 +1,56 @@
 import React from 'react'
 
+// Learn list rendering 
+
+
 function NameList() {
     const names = ['Ahmad', 'Budi', 'Cici']
+
+    //Use this code for rendering list of data (in this case names array)
     const nameList = names.map(name => <h2>{name}</h2>)
-  return (
-    <div>
-     {
-        nameList
-     }
-    </div>
-  )
+
+    /*
+    return (
+    <div>{nameList}</div>
+    )
+    */
+
+    //another example
+    const persons = [
+        {
+          "id": 1,
+          "name": "John Doe",
+          "age": 28,
+          "skill": "Programming"
+        },
+        {
+          "id": 2,
+          "name": "Jane Smith",
+          "age": 35,
+          "skill": "Data Analysis"
+        },
+        {
+          "id": 3,
+          "name": "Bob Johnson",
+          "age": 22,
+          "skill": "Graphic Design"
+        },
+        {
+          "id": 4,
+          "name": "Alice Williams",
+          "age": 31,
+          "skill": "Project Management"
+        },
+        {
+          "id": 5,
+          "name": "Sam Brown",
+          "age": 25,
+          "skill": "Customer Support"
+        }
+      ]
+      
+    const personList = persons.map(person => <h3>Hello, my name is {person.name}, i am {person.age} years old.</h3>)
+    return personList
 }
 
 export default NameList
