b217389d56e82cf24ab5bd21620660c54fba90dd
RayhanAdii <rayhan.a.wicaksono@gmail.com>
2023-08-14 22:39:48 +0700

    learn index as key

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -5,10 +5,18 @@ import Person from './Person'
 
 
 function NameList() {
-    const names = ['Ahmad', 'Budi', 'Cici']
+    const names = ['Ahmad', 'Budi', 'Cici', 'Ahmad']
 
     //Use this code for rendering list of data (in this case names array)
-    const nameList = names.map(name => <h2>{name}</h2>)
+    //const nameList = names.map((name) => <h2>{name}</h2>)
+    
+    
+    // Using index as key
+    //Only use index as key in this one of three condition
+    //1: Items on list do not have unique id
+    //2: List is static (will not change)
+    //3: List will never be reordered or filttered
+    const nameList = names.map((name,index) => <h2 key={index}>{name}</h2>)
 
     /*
     return (
@@ -17,6 +25,7 @@ function NameList() {
     */
 
     //another example
+    /*
     const persons = [
         {
           "id": 1,
@@ -53,8 +62,9 @@ function NameList() {
       const personList = persons.map(person => 
         <Person key={person.id} person={person} />
     );
+    */
     
-    return <div>{personList}</div>;
+    return <div>{nameList}</div>;
 }
 
 export default NameList
