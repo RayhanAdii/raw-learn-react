d3a6cbe047637db93faa884b022b872c400cf991
RayhanAdii <rayhan.a.wicaksono@gmail.com>
2023-08-15 10:39:09 +0700

    learn css styling in react: multiple classes, file : Stylesheet.js, myStyles.css

--- hello-world/src/components/Stylesheet.js
+++ hello-world/src/components/Stylesheet.js
@@ -4,10 +4,19 @@ import './myStyles.css'
 function Stylesheet(props) {
     let className = props.primary ? 'primary' : ''
   return (
+
+    //For one class
+    /*
     <div>
       <h1 className={className}>Stylesheet</h1>
     </div>
-  )
+    */
+
+    //For multiple classes
+    <div>
+      <h1 className={`${className} font-xl`}>Stylesheet</h1>
+    </div>
+  );
 }
 
 export default Stylesheet
