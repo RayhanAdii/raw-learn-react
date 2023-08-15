af155b5c1cd382ac80b16b8740ac3d37b3cefd15
RayhanAdii <rayhan.a.wicaksono@gmail.com>
2023-08-15 22:03:19 +0700

    learn form handling in react, file: form.js

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,70 +1,15 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
-import './appStyles.css'
-import styles from './appStyles.module.css'
+import Form from './components/Form.js'
 
-
-function App() {  
-  return (
-    <div className="App">
-    {/* Add classname in regular stylesheet */}
-      <h1 className='error'>Error</h1>
-    {/* Add classname in css module stylesheet */}
-    {/* Menghindari conflict css, kalo misal pake regular, classnamenya tetep bisa dipake di child component*/}
-      <h1 className={styles.success}>Success</h1>
-
-
-    {/*
-    <Inline></Inline>
-
-
-    <Stylesheet primary={true}></Stylesheet>
-
-    <NameList/>
-
-    <UserGreeting></UserGreeting>
-
-    <ParentComponent/>
-
-
-
-      <EventBind></EventBind>
-
-      <FunctionClick></FunctionClick>
-      <ClassClick></ClassClick>
-
-      <Counter/>
-
-      <Message></Message>
-
-      <Greet name ="Rayhan" heroName ="RayhanMan">
-        Masnya ini adalah MC
-      </Greet>
-      <Greet name ="Handoko" heroName ="Handokoman"/>
-      <Greet name = "kipli" heroName ="Peashooter"/>
-
-      <Welcome name ="Rayhan" heroName ="RayhanMan"/>
-      <Welcome name ="Handoko" heroName ="Handokoman"/>
-      <Welcome name = "kipli" heroName ="Peashooter"/>
-      
-      <Hello/>
-    */}
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Form></Form>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
