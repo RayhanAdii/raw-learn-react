41236851fbf275aac6f972a915df04cd8f065f54
RayhanAdii <rayhan.a.wicaksono@gmail.com>
2023-08-16 22:27:40 +0700

    learn intro to lifecycle, no file

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+        {/* Habis belajar pengantar lifecycle method*/}
         <Form></Form>
       </div>
     )
