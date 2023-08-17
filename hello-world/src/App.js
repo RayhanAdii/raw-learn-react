53267920dbecac0ebffd592431e25ce0a93b545f
RayhanAdii <rayhan.a.wicaksono@gmail.com>
2023-08-17 21:07:44 +0700

    learn react fragment, file: FragmentDemo, Table, Columns

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Form from './components/Form.js'
 import LifecycleA from './components/LifecycleA';
 import LifecycleAUpdate from './components/LifecycleAUpdate';
+import FragmentDemo from './components/FragmentDemo';
+import Table from './components/Table';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        
+
+      <Table/>
+
+      {/*
+        <FragmentDemo/>
+
+
         <LifecycleAUpdate/>
-        {/*
+
         <LifecycleA/>
 
          Habis belajar pengantar lifecycle method
