2ffb4f9b8327ae3d481576b0fbebe5bd0e32df8e
RayhanAdii <rayhan.a.wicaksono@gmail.com>
2023-08-19 13:21:09 +0700

    learn forwarding refs, file: FRInput, FRParentInput

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -9,15 +9,20 @@ import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
+import FRInput from './components/FRInput';
+import FRParentInput from './components/FRParentInput';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
 
-        <FocusInput/>
+        <FRParentInput/>
 
       {/*
+        <FocusInput/>
+
+
       <RefsDemo/>
 
 
